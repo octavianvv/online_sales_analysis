7578ab4fe508eb2809c50876f942fe193724c951
octavianvv <octavianvv@yahoo.com>
2025-02-09 22:34:04 +0200

    Adding the Cart class: -Adding products to cart -Calculating the total value of the basket -Calculation of the payment amount -Displaying the contents of the cart

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
+import random
 
-product_manager = ProductManager()
+def main():
+    product_manager = ProductManager()
 
-product_manager.add_product(Product("Laptop", 1200.99, 5))
-product_manager.add_product(Product("Mobile", 500.99, 10))
-product_manager.add_product(Product("Tablet", 700.99, 3))
-product_manager.add_product(Product("Camera", 1000.99, 2))
-product_manager.add_product(Product("Headphones", 200.99, 8))
+    product_manager.add_product(Product("Laptop", 1200.99, 5))
+    product_manager.add_product(Product("Mobile", 500.99, 10))
+    product_manager.add_product(Product("Tablet", 700.99, 3))
+    product_manager.add_product(Product("Camera", 1000.99, 2))
+    product_manager.add_product(Product("Headphones", 200.99, 8))
 
-product_manager.display_all_products()
-print("Total value of products:", product_manager.total_value_of_products())
-#product_manager.product_removal("Camera")
+    product_manager.display_all_products()
+    print("Total value of products:", product_manager.total_value_of_products())
+    #product_manager.product_removal("Camera")
+
+    
+    cart=Cart()
+    products=product_manager.products
+    for _ in range(3):
+        product=random.choice(products)
+        max_quantity=min(product.quantity,3)
+        
+        if max_quantity>0:
+            quantity=random.randint(1,max_quantity)
+            cart.add_to_cart(product,quantity)
+
+    cart.display_cart()
+    #cart.total_price()
+    cart_total=cart.total_price()
+    print(f"\nTotal Cart Value: {cart_total:.2f}")
+
+if __name__ == "__main__":
+    main()
